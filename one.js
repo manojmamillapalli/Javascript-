4e0a9b785b22e5c993388e162190f59091d645e3
user name in git <manojmamillapalli156@gmail.com>
2024-08-01 11:15:44 +0530

    first commit

--- one.js
+++ one.js
@@ -1,2 +1,11 @@
 // data types
 //primitive datatypes
+//null, undefined, Number, BigInt, String,Boolean
+//typeof()--operator to check data type
+var a;
+a=35;
+console.log(a);
+console.log(typeof(a));
+var b="Hello world";
+console.log(b);
+console.log(typeof(b));
