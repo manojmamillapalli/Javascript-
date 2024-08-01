b8694a0f1218850236a6e1119a3d26a4adf05970
user name in git <manojmamillapalli156@gmail.com>
2024-08-01 11:18:56 +0530

    first commit

--- one.js
+++ one.js
@@ -2,10 +2,24 @@
 //primitive datatypes
 //null, undefined, Number, BigInt, String,Boolean
 //typeof()--operator to check data type
+
 var a;
 a=35;
 console.log(a);
 console.log(typeof(a));
+
+
 var b="Hello world";
 console.log(b);
 console.log(typeof(b));
+
+
+var c=true;
+console.log(c);
+console.log(typeof(c));
+
+
+
+var d=null; // even though null value is primitive it stores in the form of object.
+console.log(d);
+console.log(typeof(d));
