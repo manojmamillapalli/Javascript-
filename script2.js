b06891cb4c66223e061e7641b183bcc59e59b731
user name in git <manojmamillapalli156@gmail.com>
2024-08-02 12:10:25 +0530

    first commit

--- script2.js
+++ script2.js
@@ -19,3 +19,23 @@ for(var x in object)
 {
     console.log(object[x]);
 }
+
+
+// foe of loop in array and object
+
+var arr=[20,30,40,50];
+for(var x of arr) //here the x value stores the values of the array FileSystemDirectoryHandle. rather than indexes
+{
+    console.log(x);
+}
+
+var number1=
+{
+    name:"pinakini",
+    number:12345,
+    place:"chennai"
+}
+for(var x of number1)
+{
+    console.log(x);// for objects the for of loop is not iterable
+}
